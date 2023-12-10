a98e4ea727f77fafb1aa32daab69f2010bc285c1
John O'Reilly <oreillyj@gmail.com>
2023-12-10 19:32:26 +0000

    ios client updates

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -6,9 +6,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.ComposeUIViewController
 import dev.johnoreilly.climatetrace.remote.ClimateTraceApi
 import dev.johnoreilly.climatetrace.remote.Country
+import dev.johnoreilly.climatetrace.remote.CountryAssetEmissionsInfo
+import dev.johnoreilly.climatetrace.remote.CountryEmissionsInfo
+import dev.johnoreilly.climatetrace.ui.CountryInfoDetailedView
 import dev.johnoreilly.climatetrace.ui.CountryListView
 
-fun CountryListViewController(onCountryClicked: (countryCode: String) -> Unit) = ComposeUIViewController {
+fun CountryListViewController(onCountryClicked: (country: Country) -> Unit) = ComposeUIViewController {
     val climateTraceApi = remember { ClimateTraceApi() }
     var countryList by remember { mutableStateOf(emptyList<Country>()) }
     var selectedCountry by remember { mutableStateOf<Country?>(null) }
@@ -19,6 +22,21 @@ fun CountryListViewController(onCountryClicked: (countryCode: String) -> Unit) =
 
     CountryListView(countryList, selectedCountry) {
         selectedCountry = it
-        onCountryClicked(it.alpha3)
+        onCountryClicked(it)
     }
+}
+
+
+fun CountryInfoDetailedViewController(country: Country) = ComposeUIViewController {
+    val climateTraceApi = remember { ClimateTraceApi() }
+    var countryEmissionInfo by remember { mutableStateOf<CountryEmissionsInfo?>(null) }
+    var countryAssetEmissons by remember { mutableStateOf<List<CountryAssetEmissionsInfo>?>(null) }
+
+    LaunchedEffect(country) {
+        val countryEmissionInfoList = climateTraceApi.fetchCountryEmissionsInfo(country.alpha3)
+        countryEmissionInfo = countryEmissionInfoList[0]
+        countryAssetEmissons = climateTraceApi.fetchCountryAssetEmissionsInfo(country.alpha3)[country.alpha3]
+    }
+
+    CountryInfoDetailedView(country, countryEmissionInfo, countryAssetEmissons)
 }
