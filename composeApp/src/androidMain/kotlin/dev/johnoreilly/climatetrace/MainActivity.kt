86b79b3228c781b9c10f749043275cf167ca4a3c
John O'Reilly <oreillyj@gmail.com>
2024-04-30 02:52:18 +0100

    remove redundant code

--- composeApp/src/androidMain/kotlin/dev/johnoreilly/climatetrace/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/dev/johnoreilly/climatetrace/MainActivity.kt
@@ -5,26 +5,12 @@ package dev.johnoreilly.climatetrace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import cafe.adriel.voyager.core.screen.Screen
-import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.Navigator
-import cafe.adriel.voyager.navigator.currentOrThrow
 import dev.johnoreilly.climatetrace.di.initKoin
-import dev.johnoreilly.climatetrace.ui.CountryEmissionsScreen
-import dev.johnoreilly.climatetrace.ui.CountryListView
-import dev.johnoreilly.climatetrace.viewmodel.ClimateTraceViewModel
-import org.koin.compose.koinInject
+import dev.johnoreilly.climatetrace.ui.CountryListScreen
 
 class MainActivity : ComponentActivity() {
 
@@ -45,35 +31,3 @@ class MainActivity : ComponentActivity() {
 fun AndroidApp() {
     Navigator(screen = CountryListScreen())
 }
-
-
-class CountryListScreen : Screen {
-    @Composable
-    override fun Content() {
-        val navigator = LocalNavigator.currentOrThrow
-
-        val viewModel = koinInject<ClimateTraceViewModel>()
-        val countryList = viewModel.countryList.collectAsState()
-        val selectedCountry = viewModel.selectedCountry.collectAsState()
-        val isLoadingCountries by viewModel.isLoadingCountries.collectAsState()
-
-        Scaffold(
-            topBar = {
-                CenterAlignedTopAppBar(title = {
-                    Text("ClimateTraceKMP")
-                })
-            }
-        ) {
-            Column(Modifier.padding(it)) {
-                CountryListView(
-                    countryList.value,
-                    selectedCountry.value,
-                    isLoadingCountries
-                ) { country ->
-                    navigator.push(CountryEmissionsScreen(country))
-                }
-            }
-        }
-    }
-}
-
