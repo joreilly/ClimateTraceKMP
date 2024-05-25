be032b15262710c88cca5cd24e4517d0ec33e47f
John O'Reilly <oreillyj@gmail.com>
2024-05-25 18:21:43 +0100

    Kotlin 2.0 + related dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,22 +1,23 @@
-rootProject.name = "ClimateTraceKMP"
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
 pluginManagement {
     repositories {
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        google()
-        gradlePluginPortal()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
+        gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
     }
 }
 
+rootProject.name = "ClimateTraceKMP"
 include(":composeApp")
