4e71732b06cc7dbea2667eb6ef7c8ce2923dcdd2
John O'Reilly <oreillyj@gmail.com>
2025-06-29 10:34:44 +0100

    Initial ADK agent

--- agents/src/main/kotlin/main.kt
+++ agents/src/main/kotlin/main.kt
@@ -1,11 +1,3 @@
 import dev.johnoreilly.climatetrace.di.initKoin
-import kotlinx.coroutines.runBlocking
-import kotlin.uuid.ExperimentalUuidApi
-
 
 val koin = initKoin(enableNetworkLogs = true).koin
-
-@OptIn(ExperimentalUuidApi::class)
-fun main() = runBlocking{
-    runKoogAgent()
-}
