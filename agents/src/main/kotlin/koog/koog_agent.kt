f64e70be9b40cdd7d3ec711bfcf414428601cac8
John O'Reilly <oreillyj@gmail.com>
2025-08-17 11:09:56 +0100

    koog agent updates

--- agents/src/main/kotlin/koog/koog_agent.kt
+++ agents/src/main/kotlin/koog/koog_agent.kt
@@ -1,7 +1,6 @@
 package koog
 
 import ai.koog.agents.core.agent.AIAgent
-import ai.koog.agents.core.tools.Tool
 import ai.koog.agents.core.tools.ToolRegistry
 import ai.koog.agents.core.tools.reflect.asTools
 import ai.koog.agents.features.eventHandler.feature.handleEvents
@@ -9,46 +8,67 @@ import ai.koog.agents.mcp.McpToolRegistryProvider
 import ai.koog.prompt.executor.clients.google.GoogleModels
 import ai.koog.prompt.executor.clients.openai.OpenAIModels
 import ai.koog.prompt.executor.llms.all.simpleGoogleAIExecutor
+import ai.koog.prompt.executor.llms.all.simpleOllamaAIExecutor
 import ai.koog.prompt.executor.llms.all.simpleOpenAIExecutor
+import ai.koog.prompt.llm.LLMCapability
+import ai.koog.prompt.llm.LLMProvider
+import ai.koog.prompt.llm.LLModel
 import kotlinx.coroutines.runBlocking
-import kotlin.uuid.ExperimentalUuidApi
-import kotlin.uuid.Uuid
 
 val openAIApiKey = ""
 val apiKeyGoogle = ""
 
 
-@OptIn(ExperimentalUuidApi::class)
+
 fun main() = runBlocking {
+
+    val model = LLModel(
+        provider = LLMProvider.Ollama,
+        id = "gpt-oss",
+        //id = "llama3.1:8b",
+        capabilities = listOf(
+            LLMCapability.Temperature,
+            LLMCapability.Schema.JSON.Simple,
+            LLMCapability.Tools
+        ),
+    )
+
+
     val agent = AIAgent(
         //executor = simpleOpenAIExecutor(openAIApiKey),
-        executor = simpleGoogleAIExecutor(apiKeyGoogle),
+        //executor = simpleGoogleAIExecutor(apiKeyGoogle),
+        executor = simpleOllamaAIExecutor(),
         //llmModel = OpenAIModels.Chat.GPT4o,
-        llmModel = GoogleModels.Gemini1_5Pro,
+        //llmModel = GoogleModels.Gemini1_5Pro,
+        llmModel = model,
         toolRegistry = createToolSetRegistry()
     ) {
         handleEvents {
-            onToolCall { tool: Tool<*, *>, toolArgs: Tool.Args ->
-                println("Tool called: tool ${tool.name}, args $toolArgs")
+            onToolCall { eventContext ->
+                println("Tool called: ${eventContext.tool} with args ${eventContext.toolArgs}")
             }
-
-            onAgentRunError { strategyName: String, sessionUuid: Uuid?, throwable: Throwable ->
-                println("An error occurred: ${throwable.message}\n${throwable.stackTraceToString()}")
+            onAgentRunError { eventContext ->
+                println("An error occurred: ${eventContext.throwable.message}\n${eventContext.throwable.stackTraceToString()}")
             }
-
-            onAgentFinished { strategyName, result ->
-                println("Agent (strategy = $strategyName) finished with result: $result")
+            onAgentFinished { eventContext ->
+                println("Agent finished with result: ${eventContext.result}")
             }
         }
     }
 
-    agent.run(
+
+    val output = agent.run(
         """
         Get emission data for France and Germany for 2023 and 2024.
-        Use units of millions for the emissions data.
+        Also break down by sector.
+        Use 3 letter country codes.
+        Use units of millions for the emissions data.        
         """.trimIndent()
     )
+
+    println(output)
 }
+
 
 
 suspend fun createToolSetRegistry(): ToolRegistry {
@@ -61,6 +81,6 @@ suspend fun createToolSetRegistry(): ToolRegistry {
     val localToolSetRegistry = ToolRegistry { tools(ClimateTraceTool().asTools()) }
 
     // Can use either local toolset or one based on MCP server
-    //return toolRegistryClimateTrace
-    return localToolSetRegistry
+    return toolRegistryClimateTrace
+    //return localToolSetRegistry
 }
