84eeda4f8576ea0cd65f912908e36f9692ea3249
John O'Reilly <oreillyj@gmail.com>
2025-11-26 10:37:27 +0000

    trying out Ai Debuger with Koog agent

--- composeApp/src/jvmMain/kotlin/main.kt
+++ composeApp/src/jvmMain/kotlin/main.kt
@@ -2,13 +2,28 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import dev.johnoreilly.climatetrace.agent.ClimateTraceAgent
+import dev.johnoreilly.climatetrace.data.ClimateTraceRepository
+import dev.johnoreilly.climatetrace.di.initKoin
 
 
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "ClimateTraceKMP") {
-        App()
-    }
+val koin = initKoin(enableNetworkLogs = true).koin
+
+suspend fun main() {
+    println("hello")
+
+    val climateTraceRepository = koin.get<ClimateTraceRepository>()
+
+    val agent: ClimateTraceAgent = ClimateTraceAgent(climateTraceRepository)
+
+    agent.runAgent("What were the emissions for the UK in 2024?")
 }
+
+//fun main() = application {
+//    Window(onCloseRequest = ::exitApplication, title = "ClimateTraceKMP") {
+//        App()
+//    }
+//}
 
 @Preview
 @Composable
